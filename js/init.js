89003bd052c3564b28c74c8dc0ed4d12d34ee123
Gabriel R F <github@gabrf.com>
2015-09-09 15:44:10 -0300

    go to top

--- js/init.js
+++ js/init.js
@@ -3,5 +3,6 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.toc-wrapper').pushpin({ top: $('.toc-wrapper').offset().top });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
