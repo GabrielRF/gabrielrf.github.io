cb838b2cc8ea9a37dbbb0073ee108a3b6d735fe9
Gabriel R F <github@gabrf.com>
2015-09-10 15:49:22 -0300

    Go to top button

--- js/init.js
+++ js/init.js
@@ -6,5 +6,18 @@
     $('.toc-wrapper').pushpin({ top: $('.toc-wrapper').offset().top });
     $('.collapsible').collapsible({accordion:false});
     $('.materialboxed').materialbox();
+
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
